85fc7233fcb1f6670b5c0bca74d3a4b2087a84f7
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 02:49:45 -0300

    Create _welcome.js

---
+++ plugins/_welcome.js
@@ -0,0 +1,22 @@
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, {conn, participants, groupMetadata}) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+  let img = imagen1
+  let chat = global.db.data.chats[m.chat]
+
+  if (chat.welcome && m.messageStubType == 27) {
+    let welcome = `┌─★ *IAN BOT - MD* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
+  }
+
+  if (chat.welcome && m.messageStubType == 28) {
+    let bye = `┌─★ *IAN BOT - MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
+  }
+
+  if (chat.welcome && m.messageStubType == 32) {
+    let kick = `┌─★ *IAN BOT -MD* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
+}}
