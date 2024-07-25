e69a8c01e0f1503ef091c9c6a74751ffe6e7cffb
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 03:26:14 -0300

    Create descargar-imagen.js

---
+++ plugins/descargar-imagen.js
@@ -0,0 +1,19 @@
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!text) throw `*🚩 Uso Correcto: ${usedPrefix + command} Luffy*`;
+  conn.reply(m.chat, '🚩 *Descargando su imagen...*', m, {
+  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+  title: packname,
+  body: wm,
+  previewType: 0, thumbnail: icons,
+  sourceUrl: channel }}})
+  const res = await googleImage(text);
+  const image = await res.getRandom();
+  const link = image;
+  conn.sendFile(m.chat, link, 'error.jpg', `*🔎 Resultado De: ${text}*\n> ${textbot}`, m, null, rcanal);
+};
+handler.help = ['imagen <query>'];
+handler.tags = ['buscador', 'tools', 'descargas'];
+handler.command = /^(image|imagen)$/i;
+handler.register = true;
+export default handler;
