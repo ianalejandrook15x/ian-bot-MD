b84e6dba315afa3f65d5c3be31f67fc2c1575573
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 02:50:43 -0300

    Create ai-bard.js

---
+++ plugins/ai-bard.js
@@ -0,0 +1,22 @@
+import fetch from 'node-fetch'
+
+var handler = async (m, { text,  usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `🍟 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a LuffyBot-MD?`, m, rcanal)
+try {
+await m.react('🕒')
+conn.sendPresenceUpdate('composing', m.chat)
+var apii = await fetch(`https://aemt.me/bard?text=${text}`)
+var res = await apii.json()
+await conn.reply(m.chat, res.result, m, rcanal)
+await m.react('✅️')
+} catch (error) {
+await m.react('✖️')
+console.error(error)
+return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, rcanal)
+}}
+
+handler.command = ['bard']
+handler.help = ['bard']
+handler.tags = ['ai']
+handler.premium = false
+export default handler
