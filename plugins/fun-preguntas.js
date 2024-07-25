1076624be89b3f4b101d17b654379b908eb9ab79
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 12:50:17 -0300

    Create fun-preguntas.js

---
+++ plugins/fun-preguntas.js
@@ -0,0 +1,22 @@
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `🚩 *Ingrese un texto a preguntar*\n\nEjemplo, !${command} Hoy juega Argentina?`, m, rcanal, )
+m.react('❔')
+await delay(1000 * 1)
+m.react('❓')
+await delay(1000 * 1)
+m.react('❔')
+await delay(1000 * 1)
+
+conn.reply(m.chat, `🚩 *Preguntas - ianBot*\n\n• *Pregunta:* ${text}\n• *Respuesta:* ${['Si','Tal vez sí','Posiblemente','Probablemente no','No','Imposible','Por que haces estas preguntas','Por eso te dejo','Para que quieres saber','No te dire la respuesta'].getRandom()}`, m, rcanal, )
+
+}
+handler.help = ['pregunta']
+handler.tags = ['fun']
+handler.command = /^pregunta|preguntas|apakah$/i
+
+handler.register = true
+
+export default handler
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
