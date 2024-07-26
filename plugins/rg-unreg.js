a2068c4bf15b66190394e885cff6642ddbc0dba5
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 22:50:14 -0300

    Create rg-unreg.js

---
+++ plugins/rg-unreg.js
@@ -0,0 +1,14 @@
+
+let handler = async (m, { conn, text }) => {
+
+let user = global.db.data.users[m.sender]
+
+user.registered = false
+return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+
+}
+handler.help = ['unreg']
+handler.tags = ['rg']
+handler.command = /^unreg(ister)?$/i
+handler.register = true
+export default handler
