0b8fc74263da22bd817a161f2878b2e41200e8b8
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 22:42:38 -0300

    Create owner-nuevonombrebot.js

---
+++ plugins/owner-nuevonombrebot.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) return conn.reply(m.chat, `🚩 *Que Nombre Deseas Ponerme?*`, m, rcanal)
+  try {
+    await conn.updateProfileName(text)
+    return conn.reply(m.chat, '✅️ *Nombre Cambiado Con Éxito*', m, rcanal)
+  } catch (e) {
+    console.log(e)
+    throw `🚩 Ocurrió Un Error¡!`
+  }
+}
+handler.help = ['nuevonombre <teks>']
+handler.tags = ['owner']
+handler.command = /^(nuevonombre|nuevonombrebot|cambianombre)$/i
+
+handler.owner = true
+export default handler
