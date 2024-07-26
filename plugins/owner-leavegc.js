447628701b06aa67265867bb63b32d4e2666a779
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 22:39:29 -0300

    Create owner-leavegc.js

---
+++ plugins/owner-leavegc.js
@@ -0,0 +1,17 @@
+
+let handler = async (m, { conn, text, command }) => {
+let id = text ? text : m.chat  
+let chat = global.db.data.chats[m.chat]
+chat.welcome = false
+await conn.reply(id, `🚩 *IanBot* Abandona El Grupo, Fué Genial Estar Aquí 👋`) 
+await conn.groupLeave(id)
+try {  
+chat.welcome = true
+} catch (e) {
+await m.reply(`${fg}`) 
+return console.log(e)
+}}
+handler.command = /^(salir|leavegc|salirdelgrupo|leave)$/i
+handler.group = true
+handler.rowner = true
+export default handler
