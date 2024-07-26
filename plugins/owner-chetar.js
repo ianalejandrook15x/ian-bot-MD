a1687a57ad5a0800072f31c9cff8c6f87c9d34fa
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-25 22:34:31 -0300

    Create owner-chetar.js

---
+++ plugins/owner-chetar.js
@@ -0,0 +1,15 @@
+
+const handler = async (m, { conn }) => {
+    const user = global.db.data.users[m.sender];
+        conn.sendMessage(m.chat, {text: `🚩 *@${m.sender.split('@')[0]} Ahora tienes recursos ilimitados*`, mentions: [m.sender]}, {quoted: fkontak});
+      global.db.data.users[m.sender].money = Infinity;
+    global.db.data.users[m.sender].estrellas = Infinity;
+  global.db.data.users[m.sender].level = Infinity;
+ global.db.data.users[m.sender].exp = Infinity;
+};
+handler.help = ['cheat'];
+handler.tags = ['owner'];
+handler.command = /^(ilimitado|infiniy|chetar)$/i;
+handler.rowner = true;
+handler.fail = null;
+export default handler;
