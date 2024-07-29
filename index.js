346f5b1378416101e1a91d7277fbe77fb07a6b55
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-28 23:25:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Ian ✰');
+console.log('\n✰ Iniciando Yaemori ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('<ian\nBot-MD', {
+say('Bot Ian', {
 font: 'block',
 align: 'center',
 colors: ['white']
@@ -23,7 +23,7 @@ font: 'chrome',
 align: 'center',
 colors: ['red']
 });
-say(`Developed By • ianalejandrook15x`, {
+say(`Developed By • Ian`, {
 font: 'console',
 align: 'center',
 colors: ['yellow']
