4223bd6f46336e2df0c7864b49fff88dcab13b18
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 18:04:59 -0300

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -15,7 +15,7 @@ const url = 'https://google.com/search?q=' + encodeURIComponent(text)
 google({'query': text}).then(res => {
 let teks = `🍟 *Resultado de* : ${text}\n\n`
 for (let g of res) {
-teks += `🐢 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+teks += `🌺 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
 }
 conn.reply(m.chat, teks, m, rcanal)
 })
