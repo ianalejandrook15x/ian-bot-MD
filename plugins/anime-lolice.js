f34e01d710ffea39fec62e8949730480de246636
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 19:17:25 -0300

    Update anime-lolice.js

--- plugins/anime-lolice.js
+++ plugins/anime-lolice.js
@@ -3,7 +3,7 @@ var handler = async (m, { conn, usedprefix }) => {
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/lolice', {
 avatar: await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-}), '', '🍟 *L O L I C E* 🍟', m)
+}), '', '🌸 *L O L I C E* 🌸', m)
 }
 
 handler.help = ['lolice']
