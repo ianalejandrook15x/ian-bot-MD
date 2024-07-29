80a8012dba88f4c5adaeb526e402e7219794bded
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 19:16:15 -0300

    Update buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -2,7 +2,7 @@ import yts from 'yt-search'
 
 var handler = async (m, { text, conn, args, command, usedPrefix }) => {
 
-if (!text) return conn.reply(m.chat, `🍟 *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Nino nakano`, m, rcanal, )
+if (!text) return conn.reply(m.chat, `🌸 *Escriba el título de algún vídeo de Youtube*\n\nEjemplo, !${command} Nino nakano`, m, rcanal, )
 
 conn.reply(m.chat, wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
@@ -15,7 +15,7 @@ let results = await yts(text)
 let tes = results.all
 let teks = results.all.map(v => {
 switch (v.type) {
-case 'video': return `🍟 *Título:* 
+case 'video': return `🌸 *Título:* 
 » ${v.title}
 
 🔗 *Enlace:* 
