4dda3cea2e13922bdc1c4543391394fac0f690da
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-28 23:25:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Yaemori ✰');
+console.log('\n✰ Iniciando Ian ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
