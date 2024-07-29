6350a7bc33323b243e3f95667a1ac7b6cc6193da
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 19:19:17 -0300

    Update mods-unbanchat.js

--- plugins/mods-unbanchat.js
+++ plugins/mods-unbanchat.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🍟 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '🌸 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
 await conn.reply(m.chat, '🚩 *¡LA BOT YA FUÉ DESBANEADA EN ESTE CHAT!*', m, fake)
 }
