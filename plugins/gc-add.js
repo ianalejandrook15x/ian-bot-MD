b9220eecf2241d70f2bcc6485791bb857719c496
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 19:00:10 -0300

    Update gc-add.js

--- plugins/gc-add.js
+++ plugins/gc-add.js
@@ -8,13 +8,13 @@ let name = await conn.getName(m.sender)
 let user = global.db.data.users[who]
 let nom = conn.getName(m.sender)
 if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *Este comando está deshabilitado por mi creador*`, m, rcanal) 
-if (!text) throw `🍟 Ingrese el número de la persona que quieres añadir a este grupo.\n\n🚩 Ejemplo:\n*${usedPrefix + command}* 66666666666`
-if (text.includes('+')) throw  `🍟 Ingrese el número todo junto sin el *(+)*`
+if (!text) throw `🌸 Ingrese el número de la persona que quieres añadir a este grupo.\n\n🚩 Ejemplo:\n*${usedPrefix + command}* 66666666666`
+if (text.includes('+')) throw  `🌸 Ingrese el número todo junto sin el *(+)*`
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
 
-await conn.reply(text+'@s.whatsapp.net', `*🍟 Hola! soy IanBot-MD, Una persona te a invitado a su grupo.*\n\n*Link*\n${link}`, m, {mentions: [m.sender]})
-m.reply(`🍟 *Enviando la invitación al privado de ${nom}*\n\n*📅 ${fecha}*\n⏰ *${tiempo}*`) 
+await conn.reply(text+'@s.whatsapp.net', `*🌸 Hola! soy IanBot-MD, Una persona te a invitado a su grupo.*\n\n*Link*\n${link}`, m, {mentions: [m.sender]})
+m.reply(`🌸 *Enviando la invitación al privado de ${nom}*\n\n*📅 ${fecha}*\n⏰ *${tiempo}*`) 
 
 }
 handler.help = ['add']
