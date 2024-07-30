e934042157ecced5fc57b1fd12be75f9bee8afff
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 22:21:13 -0300

    Update gc-add.js

--- plugins/gc-add.js
+++ plugins/gc-add.js
@@ -7,8 +7,8 @@ else who = m.chat
 let name = await conn.getName(m.sender)        
 let user = global.db.data.users[who]
 let nom = conn.getName(m.sender)
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🚩 *Este comando está deshabilitado por mi creador*`, m, rcanal) 
-if (!text) throw `🌸 Ingrese el número de la persona que quieres añadir a este grupo.\n\n🚩 Ejemplo:\n*${usedPrefix + command}* 66666666666`
+if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `🍂 *Este comando está deshabilitado por mi creador*`, m, rcanal) 
+if (!text) throw `🌸 Ingrese el número de la persona que quieres añadir a este grupo.\n\n🍂 Ejemplo:\n*${usedPrefix + command}* 66666666666`
 if (text.includes('+')) throw  `🌸 Ingrese el número todo junto sin el *(+)*`
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
