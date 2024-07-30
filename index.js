7de115d2657fdda4a2f7da5bc3121f0f68062960
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 22:19:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ break;
 });
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('🚩 Error:\n', code);
+console.error('🍂 Error:\n', code);
 process.exit();
 if (code === 0) return;
 watchFile(args[0], () => {
@@ -73,7 +73,7 @@ p.emit('message', line.trim());
 }
 process.on('warning', (warning) => {
 if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🚩 Se excedió el límite de Listeners en:');
+console.warn('🍂 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
