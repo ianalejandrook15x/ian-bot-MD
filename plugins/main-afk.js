892e25a3e169680a6fb25a36ec9d485a7743b6a4
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 22:25:49 -0300

    Update main-afk.js

--- plugins/main-afk.js
+++ plugins/main-afk.js
@@ -3,7 +3,7 @@ const handler = async (m, {text}) => {
 const user = global.db.data.users[m.sender];
 user.afk = + new Date;
 user.afkReason = text;
-conn.reply(m.chat, `🚩 *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
+conn.reply(m.chat, `🍂 *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
 `, m, rcanal);
 };
 handler.help = ['afk [alasan]'];
