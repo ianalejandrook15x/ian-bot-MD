f0cdd3b3427874164e5f4a4cb74698f902811955
ianalejandrook15x <sm.iam1836@gmail.com>
2024-07-29 21:02:52 -0300

    Update mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -6,7 +6,7 @@ const handler = async (m, {conn, isAdmin, groupMetadata }) => {
   await m.react(done)
    m.reply('🚩 *¡YA TE DI ADM MI JEFE!*');
     let nn = conn.getName(m.sender);
-     conn.reply('573012482597@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     conn.reply('543876577197@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
     m.reply('🚩 Ocurrio un error.');
   }
