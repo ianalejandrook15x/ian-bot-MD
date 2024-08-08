532daf4d19878ab337218590c40bd7bd22dd1262
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-08 15:24:39 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -10,38 +10,6 @@ let staff = `🚩 *EQUIPO DE AYUDANTES*
 🌸 *Rol:* Propietario 👑
 🚩 *Número:* ${creador}
 ✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-⁖❤️꙰  *Colaboradores:*
-
-• Miguel
-🌸 *Rol:* Colaborador 🐉
-🚩 *Número:* Wa.me/543876631319
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-• Camila
-🌸 *Rol:* Animadora 🌺
-🚩 *Número:* Wa.me/543876432321
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-• Bautista
-🌸 *Rol:* Participante
-🚩 *Número:* Wa.me/543876576152
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-• Carlos
-🌸 *Rol:* Colaborador 😎
-🚩 *Número:* Wa.me/50240874961
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-• Dilan
-🌸 *Rol:* Inspector de Rangos 🎃
-🚩 *Número:* Wa.me/543876636841
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-• Bitel
-🌸 *Rol:* El Señor de la Nochee 🦇
-🚩 *Número:* Wa.me/51925730477
-✨️ *GitHub:* https://github.com/ianalejandrook15x`
 await conn.sendFile(m.chat, icons, 'nino.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
