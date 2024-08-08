eb280c87c241f1325fed1b6accae314538b0ce74
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-08 12:15:10 -0300

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -24,7 +24,7 @@ responseType: 'arraybuffer'
 })
 return res.data
 } catch (e) {
-console.log(`Error : ${e}`)
+console.log(Error : ${e})
 }}
 
 let pp = ''
@@ -33,45 +33,28 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 
 //creador y otros
 global.creador = 'Wa.me/543876577197'
-global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'Wa.me/51939041500'
+global.ofcbot = ${conn.user.jid.split('@')[0]}
+global.asistencia = 'Wa.me/543876577197'
 global.namechannel = '『✯ 𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗜𝗮𝗻 ✯』'
-global.namechannel2 = '𝗠𝗼𝗼𝗻𝗹𝗶𝗴𝗵𝘁 - 𝗧𝗲𝗮𝗺'
-global.namegrupo = '𝗚𝗿𝗼𝘂𝗽 𝗔𝗶 𝗬𝗮𝗲𝗺𝗼𝗿𝗶'
-global.namecomu = '𝗖𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗔𝗶 𝗬𝗮𝗲𝗺𝗼𝗿𝗶'
-global.namecomu2 = '𝗖𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗠𝗼𝗼𝗻𝗹𝗶𝗴𝗵𝘁'
-global.colab1 = '𝗚𝗮𝘁𝗮𝗕𝗼𝘁 & 𝗬𝗮𝗲𝗺𝗼𝗿𝗶'
-global.colab2 = '𝗠𝗼𝗼𝗻𝗟𝗶𝗴𝗵𝘁 - 𝗕𝗼𝘁𝘀'
-global.colab3 = '𝗚𝗿𝗼𝘂𝗽 𝗔𝗶 𝗬𝗮𝗲𝗺𝗼𝗿𝗶'
+global.colab1 = 'Sigueme en WhatsApp Powered @ianalejandrook15x'
 
 //Reacciones De Comandos.!
-global.rwait = '🕒'
+global.rwait = '⏱️'
 global.done = '✅'
 global.error = '✖️'
 
-//Emojis determinado de Ian
-global.emoji = '🚩'
-global.emoji2 = '🌸'
-global.emoji3 = '✨️'
-global.emoji4 = '🍭'
-global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
-
-//mensaje en espera
-global.wait = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
-global.waitt = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
-global.waittt = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...*';
-global.waitttt = '🕒 *𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼...*';
+//sms espera
+global.wait = '⏱️ 𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...';
+global.waitt = '⏱️ 𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...';
+global.waittt = '⏱️ 𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...';
+global.waitttt = '⏱️ 𝗘𝘀𝗽𝗲𝗿𝗮 𝗨𝗻 𝗠𝗼𝗺𝗲𝗻𝘁𝗼, 𝗦𝗼𝘆 𝗟𝗲𝗻𝘁𝗼 ...';
 
 //Enlaces
-var canal = 'https://whatsapp.com/channel/0029VajkZ6blXnlwPZmbuH1u'  
-var git = 'https://github.com/ianalejandrook15x' 
-var youtube = 'https://www.youtube.com/@ianalejandrook14' 
-var github = 'https://github.com/ianalejandrook15x/ianxdd' 
-var facebook = 'https://www.facebook.com/diegosaroficial?mibextid=ZbWKwL' 
-let tiktok = 'https://tiktok.com/@ian.ian271'
-let correo = 'sm.iam1836@gmail.com'
+var canal = 'https://whatsapp.com/channel/0029VajkZ6bIXnlwPZmbuH1u' 
 
-global.redes = [canal, git, youtube, github, facebook, tiktok, correo].getRandom()
+global.redes = [canal, canal2, grupofb, git, youtube, github, facebook].getRandom()
+
+global.canales = [canal, canal2].getRandom()
 
 //Imagen
 let category = "imagen"
@@ -94,22 +77,31 @@ var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
 //Fakes
-global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD }}, "participant": "0@s.whatsapp.net" }
 
-// global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "1722667544-7398800763951731654@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: 0@s.whatsapp.net, ...(false ? { remoteJid: "543876577197-120363302285079181@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: ${packname}, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
 
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363318758721861@newsletter', newsletterName: "『✯ 𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗜𝗮𝗻 ✯』", serverMessageId: -1 }
-}}, { quoted: m }
+let icono = ['https://i.pinimg.com/736x/fb/de/8d/fbde8d159039d1faab0056c411493d8a.jpg', 'https://i.pinimg.com/736x/39/9e/ab/399eabc7c6b2fd51a232634874c4c0ed.jpg'].getRandom()
 
-global.icono = [ 
-'https://i.pinimg.com/736x/39/9e/ab/399eabc7c6b2fd51a232634874c4c0ed.jpg',
-'https://i.pinimg.com/736x/0f/40/02/0f4002892a5c8f1b5105a89b56e30619.jpg',
-'https://i.pinimg.com/736x/39/9e/ab/399eabc7c6b2fd51a232634874c4c0ed.jpg',
-'https://i.pinimg.com/736x/b1/d2/6a/b1d26ac13f38db922374de81706b9769.jpg',
-'https://i.pinimg.com/736x/7b/39/00/7b390000c59a03e4199f8209150cec65.jpg',
-'https://i.pinimg.com/736x/81/20/25/8120253581cf922f381ed099f55b8046.jpg'
-].getRandom()
-
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "7398800763951731654@newsletter", serverMessageId: 100, newsletterName: '『✯ 𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗜𝗮𝗻 ✯』', }, externalAdReply: { showAdAttribution: true, title: packname, body: '🌸 ¡Super Bot De WhatsApp!', mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = {
+contextInfo: {
+isForwarded: true,
+forwardedNewsletterMessageInfo: {
+newsletterJid: "120363318758721861@newsletter,
+serverMessageId: 100,
+newsletterName: '『✯ 𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 𝗜𝗮𝗻 ✯』,
+},
+externalAdReply: {
+showAdAttribution: true,
+title: packname,
+body: 'IAN-BOT',
+mediaUrl: null,                                   description: null,                                       previewType: "PHOTO",                                        thumbnailUrl: icono,
+sourceUrl: redes,
+mediaType: 1,
+renderLargerThumbnail: false
+},
+},
+}
+}
 
 export default handler
