b3b5e21db4d56b8467ccf7a2c051a95ac614c27b
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-09 10:44:30 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -4,8 +4,8 @@ import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 //m.react('⚙️')
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-let biografia = await conn.fetchStatus('543876577197' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/5de7ae20298fcbb74d8d8.png')
+let biografia = await conn.fetchStatus('543876639332' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
 let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Creador de Ian-bot'
