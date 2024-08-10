9e9ff2737796af8a2dfca2fe8d8d197ffd8ec182
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-10 19:28:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,115 @@
-import { join, dirname } from 'path';
+import { join, dirname } from 'path'
 import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { setupMaster, fork } from 'cluster';
-import { watchFile, unwatchFile } from 'fs';
+import { fileURLToPath } from 'url'
+import { setupMaster, fork } from 'cluster'
+import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts';
-import { createInterface } from 'readline';
-import yargs from 'yargs';
-import chalk from 'chalk';
-console.log('\n✰ Iniciando Ian ✰');
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { name, description, author, version } = require(join(__dirname, './package.json'));
-const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-say('Bot Ian', {
-font: 'block',
-align: 'center',
-colors: ['white']
-});
-say(Multi Device, {
+import { createInterface } from 'readline'
+import yargs from 'yargs'
+import express from 'express'
+import chalk from 'chalk'
+import path from 'path'
+import os from 'os'
+import { promises as fsPromises } from 'fs'
+
+// https://stackoverflow.com/a/50052194
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const require = createRequire(__dirname) //Incorpora la capacidad de crear el método 'requerir'
+const { name, author } = require(join(__dirname, './package.json')) //https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
+const { say } = cfonts
+const rl = createInterface(process.stdin, process.stdout)
+
+//const app = express()
+//const port = process.env.PORT || 8080;
+
+say('The-\nMiku\nBot\nMDV5', {
 font: 'chrome',
 align: 'center',
-colors: ['red']
-});
-say(Developed By • Ian, {
+gradient: ['red', 'magenta']})
+say(Por AdrianOficial, {
 font: 'console',
 align: 'center',
-colors: ['yellow']
-});
-var isRunning = false;
-function start(file) {
-if (isRunning) return;
-isRunning = true;
-let args = [join(__dirname, file), ...process.argv.slice(2)];
+gradient: ['red', 'magenta']})
+
+var isRunning = false
+
+async function start(file) {
+if (isRunning) return
+isRunning = true
+const currentFilePath = new URL(import.meta.url).pathname
+let args = [join(__dirname, file), ...process.argv.slice(2)]
 say([process.argv[0], ...args].join(' '), {
 font: 'console',
 align: 'center',
-colors: ['green']
-});
-setupMaster({
-exec: args[0],
-args: args.slice(1),
-});
-let p = fork();
+gradient: ['red', 'magenta']
+})
+setupMaster({exec: args[0], args: args.slice(1),
+})
+let p = fork()
 p.on('message', data => {
 switch (data) {
 case 'reset':
-p.process.kill();
-isRunning = false;
-start.apply(this, arguments);
-break;
+p.process.kill()
+isRunning = false
+start.apply(this, arguments)
+break
 case 'uptime':
-p.send(process.uptime());
-break;
-}
-});
+p.send(process.uptime())
+break
+}})
+
 p.on('exit', (_, code) => {
-isRunning = false;
-console.error('🍂 Error:\n', code);
-process.exit();
-if (code === 0) return;
+isRunning = false
+console.error('⚠️ ERROR ⚠️ >> ', code)
+start('main.js'); //
+
+if (code === 0) return
 watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+unwatchFile(args[0])
+start(file)
+})})
+
+const ramInGB = os.totalmem() / (1024 * 1024 * 1024)
+const freeRamInGB = os.freemem() / (1024 * 1024 * 1024)
+const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json')
+try {
+const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8')
+const packageJsonObj = JSON.parse(packageJsonData)
+const currentTime = new Date().toLocaleString()
+let lineM = '⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ ⋯ 》'
+console.log(chalk.yellow(`╭${lineM}
+┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┊${chalk.blueBright('┊')}${chalk.yellow(🖥️ ${os.type()}, ${os.release()} - ${os.arch()})}
+┊${chalk.blueBright('┊')}${chalk.yellow(💾 Total RAM: ${ramInGB.toFixed(2)} GB)}
+┊${chalk.blueBright('┊')}${chalk.yellow(💽 Free RAM: ${freeRamInGB.toFixed(2)} GB)}
+┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┊${chalk.blueBright('┊')} ${chalk.blue.bold(🟢INFORMACIÓN :)}
+┊${chalk.blueBright('┊')} ${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
+┊${chalk.blueBright('┊')}${chalk.cyan(💚 Nombre: ${packageJsonObj.name})}
+┊${chalk.blueBright('┊')}${chalk.cyan(𓃠 Versión: ${packageJsonObj.version})}
+┊${chalk.blueBright('┊')}${chalk.cyan(💜 Descripción: ${packageJsonObj.description})}
+┊${chalk.blueBright('┊')}${chalk.cyan(😺 Project Author: ${packageJsonObj.author.name} (The-DiosBot-MD))}
+┊${chalk.blueBright('┊')}${chalk.blueBright('┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
+┊${chalk.blueBright('┊')}${chalk.yellow(💜 Colaboradores:)}
+┊${chalk.blueBright('┊')}${chalk.yellow(• GataNina-Li (GataDios))}
+┊${chalk.blueBright('┊')}${chalk.yellow(• Sakura-OFC (S-OFC))}
+┊${chalk.blueBright('┊')}${chalk.yellow(• Alex-OFC (Alex-OFC))}
+┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
+┊${chalk.blueBright('╭┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')}
+┊${chalk.blueBright('┊')}${chalk.cyan(⏰ Hora Actual :)}
+┊${chalk.blueBright('┊')}${chalk.cyan(${currentTime})}
+┊${chalk.blueBright('╰┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅')} 
+╰${lineM}`));
+setInterval(() => {}, 1000)
+} catch (err) {
+console.error(chalk.red(❌ No se pudo leer el archivo package.json: ${err}))
+}
+
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
 if (!opts['test'])
 if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim());
-});
-}
-process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🍂 Se excedió el límite de Listeners en:');
-console.warn(warning.stack);
-}
-});
-start('mini.js');
+p.emit('message', line.trim())
+})}
+
+start('main.js')
