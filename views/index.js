636455de3e1febcf7c29cb429d63f2546a631a18
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-18 12:21:21 -0300

    Update index.js

--- views/index.js
+++ views/index.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-  const chat = document.querySelector('div.container-fluid');
-  function addMsg(obj) {
-    const html = document.createElement('span');
-    html.className = 'msg';
-    html.innerHTML = obj;
-    chat.appendChild(html);
-  }
+let chat = document.querySelector('div.container-fluid')
+function addMsg(obj) {
+  let html = document.createElement('span')
+  html.className = 'msg'
+  html.innerHTML = obj
+  chat.appendChild(html)
+}
 
-  window.onclick = () => addMsg(12);
-};
+window.onclick = () => addMsg(12)
+}
