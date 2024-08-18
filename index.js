486b362f8efb69b66db9b2babdfae8356c18def4
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-18 00:06:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ colors: ['red']
 say(`Developed By • ianalejandrook15x`, {
 font: 'console',
 align: 'center',
-colors: ['pink']
+colors: ['magenta']
 });
 var isRunning = false;
 function start(file) {
@@ -77,4 +77,4 @@ console.warn('🚩 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });
-start('main.js');
+start('start.js');
