344505537cc714c9316e7998f8d163827af9257a
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-18 14:42:26 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,16 +7,16 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = `┌─★ *IAN BOT - MD* \n│「 Bienvenido 」\n└┬❀ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🏵  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+    let welcome = `┌─★ *IAN BOT - MD* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌺  Bienvenido/a\n   │🌺  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = `┌─★ *IAN BOT - MD* \n│「 ADIOS 🌸 」\n└┬❀ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🏵  Se fue\n   │🍄 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
+    let bye = `┌─★ *IAN BOT - MD* \n│「 ADIOS 🌸 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌺  Se fue\n   │🍄 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = `┌─★ *IAN BOT -MD* \n│「 ADIOS 🌸 」\n└┬❀ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🏵  Se fue\n   │🍄 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
+    let kick = `┌─★ *IAN BOT -MD* \n│「 ADIOS 🌸 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌺  Se fue\n   │🍄 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
 await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
 }}
