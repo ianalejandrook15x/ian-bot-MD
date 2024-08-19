09424346e11277639aff373743860ab35e2c8385
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-18 21:43:39 -0300

    Update bot-audios.js

--- plugins/bot-audios.js
+++ plugins/bot-audios.js
@@ -13,7 +13,7 @@ let vn = 'https://qu.ax/xynz.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: fkontak })}  
 
-if (/^Negro|corte|trabajo|🫂$/i.test(m.text) && chat.audios) {  
+if (/^Negro|hermoso|trabajo|🫂$/i.test(m.text) && chat.audios) {  
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
 let vn = 'https://qu.ax/ExSQ.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
