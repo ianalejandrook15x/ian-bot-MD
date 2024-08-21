3f7f11cbadee1af1f9b72bfe93d22a4e357a8ca4
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-21 16:52:40 -0300

    Update ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -1,4 +1,76 @@
-import fetch from 'node-fetch';
+
+import axios from 'axios'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
+const username = `${conn.getName(m.sender)}`
+const basePrompt = `Tu nombre es Ian Bot y parece haber sido creado por ianalejandrook15x. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertido, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+if (isQuotedImage) {
+const q = m.quoted
+const img = await q.download?.()
+if (!img) {
+console.error('🚩 Error: No image buffer available')
+return conn.reply(m.chat, '☁ Error: No se pudo descargar la imagen.', m, fake)}
+const content = '☁ ¿Qué se observa en la imagen?'
+try {
+const imageAnalysis = await fetchImageBuffer(content, img)
+const query = '🍁 Descríbeme la imagen y detalla por qué actúan así. También dime quién eres'
+const prompt = `${basePrompt}. La imagen que se analiza es: ${imageAnalysis.result}`
+const description = await luminsesi(query, username, prompt)
+await conn.reply(m.chat, description, m, fake)
+} catch (error) {
+console.error('🍁 Error al analizar la imagen:', error)
+await conn.reply(m.chat, '🍁 Error al analizar la imagen.', m, fake)}
+} else {
+if (!text) { return conn.reply(m.chat, `🌸 *Ingrese su petición*\n☁ *Ejemplo de uso:* ${usedPrefix + command} Como hacer un barco de papel`, m, rcanal)}
+await m.react('💬')
+try {
+const query = text
+const prompt = `${basePrompt}. Responde lo siguiente: ${query}`
+const response = await luminsesi(query, username, prompt)
+await conn.reply(m.chat, response, m, fake)
+} catch (error) {
+console.error('🍁 Error al obtener la respuesta:', error)
+await conn.reply(m.chat, 'Error: intenta más tarde.', m, fake)}}}
+
+handler.help = ['chatgpt <texto>', 'ia <texto>']
+handler.tags = ['ai']
+handler.register = true
+// handler.estrellas = 1
+handler.command = ['ia', 'chatgpt']
+
+export default handler
+
+// Función para enviar una imagen y obtener el análisis
+async function fetchImageBuffer(content, imageBuffer) {
+try {
+const response = await axios.post('https://lumin-ai.xyz/', {
+content: content,
+imageBuffer: imageBuffer 
+}, {
+headers: {
+'Content-Type': 'application/json' 
+}})
+return response.data
+} catch (error) {
+console.error('Error:', error)
+throw error }}
+// Función para interactuar con la IA usando prompts
+async function luminsesi(q, username, logic) {
+try {
+const response = await axios.post("https://lumin-ai.xyz/", {
+content: q,
+user: username,
+prompt: logic,
+webSearchMode: false
+})
+return response.data.result
+} catch (error) {
+console.error('🍁 Error al obtener:', error)
+throw error }}
+
+/*import fetch from 'node-fetch';
 import axios from 'axios';
 import translate from '@vitalets/google-translate-api';
 import {Configuration, OpenAIApi} from 'openai';
@@ -6,7 +78,7 @@ const configuration = new Configuration({organization: global.openai_org_id, api
 const openaiii = new OpenAIApi(configuration);
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) return conn.reply(m.chat, `🌸 *Ingrese su petición*\n🚩 *Ejemplo de uso:* ${usedPrefix + command} Como hacer un avión de papel`, m, rcanal)  
+if (!text) return conn.reply(m.chat, `🌸 *Ingrese su petición*\n☁ *Ejemplo de uso:* ${usedPrefix + command} Como hacer un barco de papel`, m, rcanal)  
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat);
@@ -29,4 +101,4 @@ handler.register = true
 handler.estrellas = 5
 handler.command = ['ia', 'chatgpt']
 
-export default handler;
+export default handler;*/
