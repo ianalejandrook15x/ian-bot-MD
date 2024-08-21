0605023c28e8aa99ed1c604d27b4eb8ee2b2ed1a
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-21 12:29:56 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -157,7 +157,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     
 let urls = [
 "https://telegra.ph/file/8f4158290c1263fe285ca.mp4",
-"https://telegra.ph/file/0571d52d1e88fa0fb0b68.mp4",
+"https://qu.ax/EBdw.mp4",
+"https://qu.ax/czUX.mp4",
 ];
 let gifUrl = urls[Math.floor(Math.random() * urls.length)];
 await conn.sendMessage(m.chat, {video: {url: gifUrl}, gifPlayback: true, caption: text.trim(), mentions: [m.sender]}, {quoted: m});
