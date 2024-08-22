ad3fcab28822910a6a0106e6460d60fd14be312f
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-22 16:11:51 -0300

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -6,7 +6,7 @@ const handler = async (m, {usedPrefix}) => {
   const name = conn.getName(who);
   m.reply(`╭━〔  ${global.packname}  〕⬣
 ┋🚩 *Usuario:* ${name}
-┋🌟 *Estrellas*: ${global.db.data.users[who].estrellas} 🌟
+┋💵 *Dolares*: ${global.db.data.users[who].estrellas} 💵
 ┋💰 *Experiencia:* ${global.db.data.users[who].exp}
 ╰━━━━━━━━━━━━⬣
 
