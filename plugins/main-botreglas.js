e690bc84cfe06975435a53a1b477d9b9e1fc1277
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-24 23:21:57 -0300

    Update and rename main-botreglas.ja to main-botreglas.js

--- plugins/main-botreglas.js
+++ plugins/main-botreglas.js
@@ -1,17 +1,16 @@
-
 let handler = async (m, { conn, usedPrefix, command}) => {
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-let Ian = `🍂 *Respeta las reglas de IanBot*\n
+let yaemori = `🍁 *Respeta las reglas de IanBot*\n
 ✰ No llamar Al Bot
 ✰ No hacer spam
 ✰ Contacta al creador si es necesario
 ✰ Pedir permiso para añadir al bot a un grupo
 
-🍬 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
+🌸 𝗡𝗼𝘁𝗮: *Si no cumples con las reglas del bot, seras bloqueado.*
 
-⚠️ 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Ian.*
+🌸 𝗔𝘃𝗶𝘀𝗼: *Puedes apoyarnos dejando una estrellita 🌟 al repositorio oficial de Ian.*
 
 ${global.md}`.trim()
 await conn.reply(m.chat, ian, m, fake)
