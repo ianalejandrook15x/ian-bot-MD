8ea1e08eed2103f0987effd88b90cf01bc41f33d
Dev Diego <151096440+Dev-Diego@users.noreply.github.com>
2024-09-03 09:40:16 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando Ian ✰');
+console.log('\n✰ Iniciando IaBot ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
