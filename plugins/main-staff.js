75f0ced99dbd76498b110c26ee4d9fef89d0641b
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-02 17:40:16 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,42 +1,42 @@
-
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
-🌸 *Bot:* ${global.botname}
-✨️  *Versión:* ${global.vs}
-
-👑 *Propietario:*
-
-• Ianalejandrook15x
-🌸 *Rol:* Propietario 👑
-🚩 *Número:* ${creador}
-✨️ *GitHub:* https://github.com/ianalejandrook15x
-
-🌸COLABORADORES
-
-• Miguel
-🌸 *Rol:* Desarrollador ☁
-🚩 *Número:* 543876631319
-✨️ *GitHub:* ---
-await conn.sendFile(m.chat, icons, 'nino.jpg', staff.trim(), fkontak, true, {
-contextInfo: {
-'forwardingScore': 200,
-'isForwarded': false,
-externalAdReply: {
-showAdAttribution: true,
-renderLargerThumbnail: false,
-title: `🥷 Developers 👑`,
-body: `🚩 Staff Oficial`,
-mediaType: 1,
-sourceUrl: redes,
-thumbnailUrl: icono
-}}
-}, { mentions: m.sender })
-m.react(emoji)
-
-}
-handler.help = ['staff']
-handler.command = ['colaboradores', 'staff']
-handler.register = true
-handler.tags = ['main']
-
-export default handler
+    let staff = `EQUIPO DE AYUDANTES DEL BOT
+    🌸 *Bot:* ${botname}
+    🌸 *Versión:* ${vs}
+    🌸 *Libreria:* ${libreria + baileys}
+    
+    👑 *Pɾσριҽƚαɾισ:*
+    
+    • 
+    ☁ *Rol:* ιαɳαʅҽʝαɳԃɾσσƙ15x
+    ☁ *Número:* ${creador}
+    ☁ *GitHub:* https://github.com/ianalejandrook15x
+    
+    🍄  *Colaboradores:*
+    
+    • Dev-Diego
+    ☁ *Rol:* Colaborador
+    ☁ *Número:* Wa.me/573012482597
+    ☁ *GitHub:* https://github.com/Dev-Diego`
+    await conn.sendFile(m.chat, icons, 'nino.jpg', staff.trim(), fkontak, true, {
+    contextInfo: {
+    'forwardingScore': 200,
+    'isForwarded': false,
+    externalAdReply: {
+    showAdAttribution: true,
+    renderLargerThumbnail: false,
+    title: `👑 Developers 👑`,
+    body: `🌙 STAFF DEL BOT`,
+    mediaType: 1,
+    sourceUrl: redes,
+    thumbnailUrl: icono
+    }}
+    }, { mentions: m.sender })
+    await m.react(emoji)
+    
+    }
+    handler.help = ['staff']
+    handler.command = ['colaboradores', 'staff']
+    handler.register = true
+    handler.tags = ['main']
+    
+    export default handler
