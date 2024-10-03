1f34353df556834d47386353bbe5de09aea2ab94
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 00:26:56 -0300

    Update descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -1,33 +1,33 @@
 import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, {conn, usedPrefix, command, text}) => {
-if (!text) return conn.reply(m.chat, '🚩 *Ingrese el nombre de la apk para descargarlo.*', m, rcanal)
+if (!text) return conn.reply(m.chat, '🌸 *Iɳɠɾҽʂҽ ҽʅ ɳσɱზɾҽ ԃҽ ʅα αρƙ ραɾα ԃҽʂƈαɾɠαɾʅσ.*', m, rcanal)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '🚩 *Descargando su aplicación...*', m, {
+conn.reply(m.chat, '🌸 *Eʂρҽɾҽ υɳ ɱσɱҽɳƚσ...*', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
-body: wm,
+body: dev,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
-let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
-txt += `🌸 *Nombre* : ${data5.name}\n`
-txt += `🚩 *Package* : ${data5.package}\n`
-txt += `🪴 *Update* : ${data5.lastup}\n`
-txt += `⚖ *Peso* :  ${data5.size}`
+let txt = `*DESCARGAS MOD APK* 乂\n\n`
+txt += `🌙 *Nombre* : ${data5.name}\n`
+txt += `🌙 *Package* : ${data5.package}\n`
+txt += `🌙 *Update* : ${data5.lastup}\n`
+txt += `🌙 *Peso* :  ${data5.size}`
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '🛑 *El archivo es demaciado pesado*', m, rcanal )}
+return await conn.reply(m.chat, '☁ *El archivo es demaciado pesado*', m, rcanal )}
 await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
 } catch {
-return conn.reply(m.chat, '🛑 *Ocurrió un fallo*', m, rcanal )}}
+return conn.reply(m.chat, '☁ *Ocurrió un fallo*', m, rcanal )}}
 
 handler.tags = ['descargas']
 handler.help = ['apkmod']
-handler.command = /^(apkmod|apk|dapk2|aptoide|aptoidedl)$/i
+handler.command = ['apk', 'modapk', 'aptoide']
 handler.register = true
 handler.estrellas = 1
 
