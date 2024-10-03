edf38dd36705d7b25a79d226071874dea990107c
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 04:07:43 -0300

    Update _antifakes.js

--- plugins/_antifakes.js
+++ plugins/_antifakes.js
@@ -1,49 +1,21 @@
-import db from '../lib/database.js'
-let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin} ) {
+var handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner }) {
+
 if (!m.isGroup) return !1
 let chat = global.db.data.chats[m.chat]
-if (isBotAdmin && chat.antifake) {
-let texto = `᥀·࣭࣪̇˖✨️◗ 𝙃𝙤𝙡𝙖 *@${m.sender.split`@`[0]}*, 𝙡𝙤𝙨 𝙣𝙪𝙢𝙚𝙧𝙤𝙨 𝙧𝙖𝙧𝙤𝙨 𝙣𝙤 𝙚𝙨𝙩𝙖𝙣 𝙥𝙚𝙧𝙢𝙞𝙩𝙞𝙙𝙤𝙨 𝙖𝙦𝙪𝙞, 𝙨𝙚𝙧𝙖𝙨 𝙚𝙡𝙞𝙢𝙞𝙣𝙖𝙙𝙤.`
-if (m.sender.startsWith('6' || '6')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('90' || '90')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('92' || '92')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('93' || '93')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('94' || '94')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('7' || '7')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('49' || '49')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('2' || '2')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('91' || '91')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
-if (m.sender.startsWith('48' || '48')) {
-global.db.data.users[m.sender].block = true
-await conn.reply(m.chat, texto, m)
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')} 
-}}
+
+if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
+let forbidPrefixes = ['965', '966', '971', '974', '212', '213', '216', '44', '1', '62', '61', '64', '353', '33', '32', '41', '352', '377', '351', '244', '258', '91', '977', '880', '92', '94', '960', '7', '380', '375', '998', '996', '373', '374', '994', '992', '62', '49', '43', '39', '378', '379', '86', '886', '852', '853', '65', '850', '82', '93', '98', '48', '84', '856', '855', '254', '255', '256', '250', '257', '258', '252', '269', '243', '90', '998', '60', '222', '27', '265']
+
+for (let prefix of forbidPrefixes) {
+if (m.sender.startsWith(prefix)) {
+m.reply('🌸 *Tu número no es bienvenido aqui, seras eliminado/a*\n\nHasta pronto', m.sender)
+await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+return false
+}}}
+
+return true
+
+}
+
 export default handler
