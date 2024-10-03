101826223dfccc59e1c71d9036d85bf68d245f6d
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 04:40:32 -0300

    Update gc-encuesta.js

--- plugins/gc-encuesta.js
+++ plugins/gc-encuesta.js
@@ -1,8 +1,7 @@
-
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `⚠️️ *_Ingrese un texto para iniciar la escuesta._*\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `⚠️️ *Ingrese un texto para iniciar la escuesta.*\n\nℹ️ Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `⚠️️ Separe las encuestas con *|* \n\nℹ️ Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
