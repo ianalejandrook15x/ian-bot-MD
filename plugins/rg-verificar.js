6a6de2e04782c3670d2d3568dcc16828663c404c
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 16:25:09 -0300

    Update rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -26,16 +26,16 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   global.db.data.users[m.sender].joincount += 5
   let sn = createHash('md5').update(m.sender).digest('hex')
 let regbot = `⋨ 🌸 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗢 🌸 ⋩\n`
-regbot += `┳━━━━━━━━━━━━━━┳\n`
-regbot += `「🌙」Nσɱზɾҽ: ${name}\n`
-regbot += `「🌙」Eԃαԃ: ${age} años\n`
-regbot += `┳━━━━━━━━━━━━━━┳\n`
-regbot += `「🌙」Rҽƈσɱρҽɳʂαʂ:\n\n`
-regbot += `🌸 15 Eʂƚɾҽʅʅαʂ🌟\n`
-regbot += `🌸 5 IαɳCσιɳʂ 🪙\n`
-regbot += `🌸 245 Exρҽɾιҽɳƈια 💸\n`
-regbot += `🌸 12 Tσƙҽɳʂ 💰\n`
-regbot += `┳━━━━━━━━━━━━━━┳\n`
+regbot += `┳━━━━━━━━━━━━━┳\n`
+regbot += `>「🌙」Nσɱზɾҽ: ${name}\n`
+regbot += `>「🌙」Eԃαԃ: ${age} años\n`
+regbot += `┳━━━━━━━━━━━━━┳\n`
+regbot += `>「🌙」Rҽƈσɱρҽɳʂαʂ:\n\n`
+regbot += `>「🌸」15 Eʂƚɾҽʅʅαʂ🌟\n`
+regbot += `>「🌸」5 IαɳCσιɳʂ 🪙\n`
+regbot += `>「🌸」245 Exρҽɾιҽɳƈια 💸\n`
+regbot += `>「🌸」12 Tσƙҽɳʂ 💰\n`
+regbot += `┳━━━━━━━━━━━━━┳\n`
 regbot += `${packname}`
 await m.react('🌸')
 await conn.sendLuffy(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, channel, m)
