228f539788a8072ad9cc6a5d680d2bbb2da31774
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-03 22:51:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,6 @@ global.imagen1 = fs.readFileSync('./Menu.jpg')
 global.fantasyVid = ['./media/menus/Menuvid1.mp4', './media/menus/Menuvid2.mp4', './media/menus/Menuvid3.mp4']
 global.imagen2 = fs.readFileSync('./Menu2.jpg')
 global.imagen3 = fs.readFileSync('./Menu3.jpg')
-global.imagen4 = fs.readFileSync('./Menu4.jpg')
 global.welcome = fs.readFileSync('./media/welcome.jpg')
 global.adios = fs.readFileSync('./media/adios.jpg')
 global.catalogo = fs.readFileSync('./storage/img/catalogo1.jpg')
