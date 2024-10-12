a5f60fb17ec41be2f5b34c4067764758ea5221c9
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-12 20:20:55 -0300

    Update rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -11,7 +11,7 @@ let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 
 let text = `
-╭───═[ *Top ${len} Estrellas 💵* ]═────⋆
+╭───═[ *Top ${len} Estrellas ⭐* ]═────⋆
 │╭───────────────···
 ││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
 ││ ${sortedLim.slice(0, len).map(({ jid, estrellas }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${estrellas} ⭐*`).join`\n││ `}
