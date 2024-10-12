2044b90cbeba124ff0f1965c72d1c5671561ae19
ianalejandrook15x <sm.iam1836@gmail.com>
2024-10-12 20:16:33 -0300

    Update rpg-cartera.js

--- plugins/rpg-cartera.js
+++ plugins/rpg-cartera.js
@@ -4,7 +4,7 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m
 if (who == conn.user.jid) return error 
 if (!(who in global.db.data.users)) return conn.reply(m.chat, '🍭 El usuario no se encuentra en mi base de Datos.', m, fake)
 let user = global.db.data.users[who]
-await m.reply(`${who == m.sender ? `Tienes *${user.estrellas} 💵 Dolares* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.estrellas} 💵 Dolares* en su Cartera`}. `, null, { mentions: [who] })}
+await m.reply(`${who == m.sender ? `Tienes *${user.estrellas} ⭐ Estrellas* en tu Cartera` : `El usuario @${who.split('@')[0]} tiene *${user.estrellas} ⭐ Estrellas* en su Cartera`}. `, null, { mentions: [who] })}
 
 handler.help = ['estrellas']
 handler.tags = ['rpg']
